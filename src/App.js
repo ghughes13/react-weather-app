157198cc04876c90b7d2b56568ef61de24331581
Garrett <guitarguy13@ymail.com>
2019-09-26 11:52:56 -0500

    Created OpenWeather API Connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+
+  const [data, setData] = useState([]);
+  let zipCode = 71111;
+  useEffect(() => {
+    axios
+      .get("http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "&APPID=6c383058dc52c8efb2a8317ed6ce95c4")
+      .then(result => setData(result.data));
+  }, []);
+  
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
-
-export default App;
